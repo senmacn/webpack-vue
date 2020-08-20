967f6424a526830364acedd100c48dd487cbedbb
chenyijin <15652581953@163.com>
2020-08-20 23:59:02 +0800

    Update index.vue
    修改blog侧边栏

--- my-project/src/views/blog/index.vue
+++ my-project/src/views/blog/index.vue
@@ -2,56 +2,32 @@
   <el-container class="s-blog-container">
     <el-aside width="200px">
       <el-menu class="el-menu-vertical blog-aside-menu">
-        <el-submenu
-          index="1"
-          text-color='#fff'
-          active-text-color:='#ffd04b'
-        >
-          <template slot="title">
-            <i class="el-icon-location"></i>
-            <span>导航一</span>
-          </template>
-          <el-menu-item-group>
-            <template slot="title">分组一</template>
-            <el-menu-item index="1-1">选项1</el-menu-item>
-            <el-menu-item index="1-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="1-3">选项3</el-menu-item>
-          </el-menu-item-group>
-          <el-submenu index="1-4">
-            <template slot="title">选项4</template>
-            <el-menu-item index="1-4-1">选项1</el-menu-item>
-          </el-submenu>
-        </el-submenu>
+        <el-menu-item index="1">
+          <i class="el-icon-s-home"></i>
+          <span slot="title">主页</span>
+        </el-menu-item>
         <el-menu-item index="2">
           <i class="el-icon-menu"></i>
-          <span slot="title">导航二</span>
+          <span slot="title">功能</span>
         </el-menu-item>
-        <el-menu-item
-          index="3"
-          disabled
-        >
-          <i class="el-icon-document"></i>
-          <span slot="title">导航三</span>
-        </el-menu-item>
-        <el-menu-item index="4">
+        <el-menu-item index="3">
           <i class="el-icon-setting"></i>
-          <span slot="title">导航四</span>
+          <span slot="title">设置</span>
         </el-menu-item>
       </el-menu>
     </el-aside>
+
     <el-container>
       <el-header
         class="s-blog-header"
-        height="200px"
-      >
+        height="200px">
 
       </el-header>
       <el-main height="auto">
 
       </el-main>
     </el-container>
+
   </el-container>
 </template>
 
